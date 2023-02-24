052d44861ea69cc23cda63827c63c8a68b022ee5
IRADUKUNDA Pacific Rugwizangoga <pacific.r.iradukunda@gmail.com>
2023-02-24 11:18:21 +0200

    add pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import Calculator from './components/calculator';
+import { Route, Routes } from 'react-router-dom';
+import Calculator from './components/pages/calculator';
+import Quote from './components/pages/Quote';
+import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
 import './App.css';
 
 const App = () => (
-  <Calculator />
+  <div>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/quote" element={<Quote />} />
+    </Routes>
+  </div>
 );
 
 export default App;
